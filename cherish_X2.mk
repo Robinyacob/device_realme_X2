2e9096d3202be4805db39c0819e2110544aefea2
Robinyacob <100703213+Robinyacob@users.noreply.github.com>
2022-04-11 18:37:35 +0530

    Update cherish_X2.mk

--- cherish_X2.mk
+++ cherish_X2.mk
@@ -19,6 +19,10 @@ PRODUCT_MANUFACTURER := realme
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Gapps Build
+
+WITH_GMS := true
+
 BUILD_FINGERPRINT := "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
